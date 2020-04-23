08f0b87c10a44b4a1815072ce9fd211440518da0
JIeCoRyI3 <jiecoryi3@gmail.com>
2020-04-23 17:16:00 +0300

    HW6 ready

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,1 +1,8 @@
-// Your code goes here
+const _100 = 100, _toFixedComa = 2;
+const checkNumber = Number.parseFloat(prompt('Enter check number')).toFixed(_toFixedComa);
+const tip = Number.parseFloat(prompt('Enter tip percentage')).toFixed(_toFixedComa);
+const tipAmount = (checkNumber * tip / _100).toFixed(_toFixedComa);
+const totalSum = (checkNumber - tipAmount).toFixed(_toFixedComa);
+const isValid = !isNaN(checkNumber) && !isNaN(tip) && totalSum >= 0 && tip >= 0 && tip <= _100;
+isValid ? alert(`Check number: ${checkNumber}\nTip: ${tip}%\nTip amount: ${tipAmount}\nTotal sum to pay: ${totalSum}`) 
+		: alert('Invalid input data');
--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,1 +1,17 @@
-// Your code goes here
+const _2 = 2;
+const inputData = prompt('Enter a word');
+let isValid;
+if(inputData) {
+	if (inputData.replace(/\s/g, '').length) {
+		isValid = true;
+	} else{
+		isValid = false;
+	}
+} else {
+	isValid = false;
+}
+const word = isValid? inputData : 'test';
+const startMiddleChar = word.length % _2 === 0 ? word.length / _2 - 1 : Math.floor(word.length / _2);
+const countMiddleChar = word.length % _2 === 0 ? _2 : 1;
+const middleChar = word.substring(startMiddleChar, startMiddleChar + countMiddleChar);
+isValid? alert(`"${middleChar}"`) : alert('Invalid value');
