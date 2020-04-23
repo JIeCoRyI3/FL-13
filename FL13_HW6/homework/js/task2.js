0c063820f2b108e3453e461575a2ac09c7cf4d7c
JIeCoRyI3 <jiecoryi3@gmail.com>
2020-04-23 17:35:18 +0300

    HW6 final result

--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -10,8 +10,12 @@ if(inputData) {
 } else {
 	isValid = false;
 }
-const word = isValid? inputData : 'test';
-const startMiddleChar = word.length % _2 === 0 ? word.length / _2 - 1 : Math.floor(word.length / _2);
-const countMiddleChar = word.length % _2 === 0 ? _2 : 1;
-const middleChar = word.substring(startMiddleChar, startMiddleChar + countMiddleChar);
-isValid? alert(`"${middleChar}"`) : alert('Invalid value');
+if(isValid){
+	const word = inputData;
+	const startMiddleChar = word.length % _2 === 0 ? word.length / _2 - 1 : Math.floor(word.length / _2);
+	const countMiddleChar = word.length % _2 === 0 ? _2 : 1;
+	const middleChar = word.substring(startMiddleChar, startMiddleChar + countMiddleChar);
+	alert(`"${middleChar}"`);
+} else {
+	alert('Invalid value');
+}
