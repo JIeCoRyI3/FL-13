768e513b0be97cfaf30b223dab7e65aa113f07e6
JIeCoRyI3 <jiecoryi3@gmail.com>
2020-04-25 18:25:52 +0300

    HW7 ready!

--- FL13_HW7/homework/src/js/task1.js
+++ FL13_HW7/homework/src/js/task1.js
@@ -1,1 +1,48 @@
-// Your code goes here
+const _HOURSMAX = 20, _HOURSMIN = 8, _LEN = 4;
+
+function onLogin() {
+	let inputData = prompt('Enter a login');
+	if(inputData) {
+		if(inputData.length < _LEN) {
+			alert('I don\'t know any users having name length less than 4 symbols');
+		} else if (inputData === 'User' || inputData === 'Admin'){
+			onPassword(inputData);
+		} else {
+			alert('I don’t know you');
+		}
+	} else {
+		alert('Canceled');
+	}
+}
+
+function onPassword(user) {
+	let password = prompt('Enter a password');
+	if(password) {
+		if(user === 'User') {
+			if (password === 'UserPass') {
+				onGreet(user);
+			} else {
+				alert('Wrong password');
+			}
+		} else {
+			if (password === 'RootPass') {
+				onGreet(user);
+			} else {
+				alert('Wrong password');
+			}
+		}
+	} else {
+		alert('Canceled');
+	}
+}
+
+function onGreet(user) {
+	const currentHours = new Date().getHours();
+	if(currentHours < _HOURSMAX && currentHours > _HOURSMIN) {
+		alert(`Good day, dear ${user}`);
+	} else {
+		alert(`Good evening, dear ${user}`);
+	}
+}
+
+onLogin();
--- FL13_HW7/homework/src/js/task2.js
+++ FL13_HW7/homework/src/js/task2.js
@@ -1,1 +1,46 @@
-// Your code goes here
+const _POW = 2, _MAXATTEMPTS = 3, _MINRANGE = 5, _MP = 25;
+let totalPrize = 0;
+let totalAttempts;
+
+function onStartGame() {
+	if(totalPrize === 0){
+		totalAttempts = 1;
+		const input = confirm('Do you want to play a game?');
+		input? onPlayGame() : alert('You did not become a billionaire, but can.');
+	} else {
+		totalAttempts++;
+		onPlayGame();
+	}
+}
+
+function onPlayGame() {
+	const randomNumber = Math.round(Math.random() * _MINRANGE * totalAttempts);
+	let attempts = _MAXATTEMPTS;
+	while(attempts) {
+		const possiblePrize = attempts === _MAXATTEMPTS? Math.pow(_POW, totalAttempts - 1) * (attempts + 1) * _MP 
+														: Math.pow(_POW, totalAttempts - 1) * attempts * _MP;
+		const attemptNumber = prompt(`Choose a roulette pocket number from ${0} to ${_MINRANGE * totalAttempts}` +
+									`\nAttempts left: ${attempts}` + 
+									`\nTotal prize: ${totalPrize}` +
+									`\nPossible prize at current attempt: ${possiblePrize}`);
+		if(attemptNumber && attemptNumber.trim() && +attemptNumber === randomNumber) {
+			return onWinner(possiblePrize);
+		}
+		attempts--;
+	}
+	return onEndGame();
+}
+
+function onWinner(prize) {
+	const input = confirm(`Congratulation, you won! Your prize is: ${prize}$. Do you want to continue?`);
+	totalPrize += prize;
+	input? onStartGame() : onEndGame();
+}
+
+function onEndGame() {
+	alert(`Thank you for your participation. Your prize is: ${totalPrize}$`);
+	totalPrize = 0;
+	onStartGame();
+}
+
+ onStartGame();
