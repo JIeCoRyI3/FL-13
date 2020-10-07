68f88272e53245cba661af0e5714988f943e1d3b
Kirill Shelipov <52535025+JIeCoRyI3@users.noreply.github.com>
2020-10-07 14:33:18 +0300

    Update index.js

--- task-11/src/components/movie-page/index.js
+++ task-11/src/components/movie-page/index.js
@@ -1,16 +1,3 @@
-import MoviePage from './MoviePage.js';
-import {withTranslation} from "../../hocs/withTranslation";
-import {compose} from "redux";
-import mapStateToProps from "./selectors";
-import {deleteMovie, selectNonMovie} from "./actions";
-import {connect} from "react-redux";
+import MoviePage from './container';
 
-const withStore = connect(mapStateToProps, {
-    deleteMovie,
-    selectNonMovie,
-});
-
-export default compose(
-    withTranslation,
-    withStore
-)(MoviePage);
+export default MoviePage;
