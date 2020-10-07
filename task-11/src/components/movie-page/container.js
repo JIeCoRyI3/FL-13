7d1f9834dc746cea5f69402b598573af95ef2599
Kirill Shelipov <52535025+JIeCoRyI3@users.noreply.github.com>
2020-10-07 14:30:00 +0300

    Update container.js

--- task-11/src/components/movie-page/container.js
+++ task-11/src/components/movie-page/container.js
@@ -1,1 +1,22 @@
+import MoviePage from './component';
+import {withTranslation} from "../../hocs/withTranslation";
+import {compose} from "redux";
+import mapStateToProps from "./selectors";
+import {deleteMovie, selectNonMovie} from "./actions";
+import {connect} from "react-redux";
 
+const MoviePageContainer = (props) => {
+  return (
+    <MoviePage ...props />
+  );
+};
+
+const withStore = connect(mapStateToProps, {
+    deleteMovie,
+    selectNonMovie,
+});
+
+export default compose(
+    withTranslation,
+    withStore
+)(MoviePage);
