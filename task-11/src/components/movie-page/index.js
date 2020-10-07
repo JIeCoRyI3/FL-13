c4453cd2a0b9516f85b179f562ea76a66d07bf09
Kirill Shelipov <52535025+JIeCoRyI3@users.noreply.github.com>
2020-10-07 14:21:01 +0300

    Update index.js

--- task-11/src/components/movie-page/index.js
+++ task-11/src/components/movie-page/index.js
@@ -1,26 +1,14 @@
 import MoviePage from './MoviePage.js';
 import {withTranslation} from "../../hocs/withTranslation";
 import {compose} from "redux";
-import {filterActors, filterSelector} from "./selectors";
-import {List} from "immutable";
+import mapStateToProps from "./selectors";
 import {deleteMovie, selectNonMovie} from "./actions";
 import {connect} from "react-redux";
 
-const mapStateToProps = (state) => {
-    const searchData = filterSelector(state);
-    const movieActors = filterActors(state);
-    return {
-        selectedMovie: searchData.toJS()[state.selectedMovie],
-        actors: List(movieActors),
-    };
-};
-
-const mapDispatchToProps = {
+const withStore = connect(mapStateToProps, {
     deleteMovie,
     selectNonMovie,
-};
-
-const withStore = connect(mapStateToProps, mapDispatchToProps);
+});
 
 export default compose(
     withTranslation,
